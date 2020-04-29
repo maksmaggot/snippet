41c11d7cf1069895070a855d6ab8d74b86ab69c0
maksmaggot <maksmaggot@gmail.com>
2020-04-29 23:07:45 +0700

    isolating routes

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 	"runtime/debug"
 )
 
@@ -19,4 +20,28 @@ func (app *application) ClientError(w http.ResponseWriter, status int) {
 
 func (app *application) NotFound(w http.ResponseWriter) {
 	app.ClientError(w, http.StatusNotFound)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+			return nil, err
+		}
+	}
+	return f, nil
 }
